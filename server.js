e0d579ab4bbb021cd4de8e03f15f25aa16f3d756
Christopher Ng <babedroid@gmail.com>
2017-09-29 02:28:35 +0000

    implement logic

--- server.js
+++ server.js
@@ -1,37 +1,88 @@
-var SparkBot = require("node-sparkbot");
-var bot = new SparkBot();
+let SparkBot = require("node-sparkbot")
+let bot = new SparkBot()
+let SparkAPIWrapper = require("node-sparkclient")
+let spark = new SparkAPIWrapper(process.env.SPARK_TOKEN)
+let axios = require('axios')
 
-process.env.SPARK_TOKEN = 'ZGUyMGE4NWQtMTY1ZC00NDYwLTg5YmMtZWVmMzA3YTExNDJmYjhlZDcwZTQtYzJi';
+var actionStates = {}
+var reports = {}
 
-var SparkAPIWrapper = require("node-sparkclient");
-if (!process.env.SPARK_TOKEN) {
-    console.log("Could not start as this bot requires a Cisco Spark API access token.");
-    console.log("Please add env variable SPARK_TOKEN on the command line");
-    console.log("Example: ");
-    console.log("> SPARK_TOKEN=XXXXXXXXXXXX DEBUG=sparkbot* node helloworld.js");
-    process.exit(1);
-}
-var spark = new SparkAPIWrapper(process.env.SPARK_TOKEN);
-
-bot.onCommand("hello", function (command) {
-    var email = command.message.personEmail; // Spark User that created the message orginally 
-    console.log(command.message);
-    spark.createMessage(command.message.roomId, "Hello <@personEmail:" + email + ">", { "markdown":true }, function(err, message) {
-        if (err) {
-            console.log("WARNING: could not post Hello message to room: " + command.message.roomId);
-            return;
+bot.onMessage((trigger, message) => {
+    if (message.personEmail != 'scrumme@sparkbot.io') {
+        if (message.text == '/scrum') {
+            var command = bot.asCommand(message);
+            if (command) {
+                spark.createMessage(command.message.roomId, "Let's start Scrum\n\n**What did you accomplish yesterday?**", { "markdown":true }, (err, message) => {
+                    if (!err) {
+                        actionStates[command.message.personEmail] = 1
+                        reports[command.message.personEmail] = {
+                            userId: 2,
+                            accomplishedTask: "",
+                            isAchievedGoal: false,
+                            goal: "",
+                            obstacle: ""
+                        }
+                    }
+                })
+            }
+        } else if (message.text == '/report') {
+            var command = bot.asCommand(message);
+            if (command) {
+                axios.get('http://afc62129.ngrok.io/api/reports/summary')
+                            .then((res) => {
+                                 spark.createMessage(command.message.roomId, `**Participation** ${res.data.percentParticipation}%\n\n**Goal completion** ${res.data.percentAccomplished}%\n\n**Blocked** ${res.data.percentObstacle}%`, { "markdown":true }, (err, message) => {})
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                            })
+            }
+        } else if ((message.text.includes("hi") || message.text.includes("hello") || message.text.includes("hey")) && message.text.includes("bot")) {
+            console.log(`greet from ${message.personEmail}`)
+            spark.createMessage(message.roomId, `Hello <@personEmail: ${message.personEmail}> :). How are you today?`, { "markdown":true }, (err, message) => {})
+        } else {
+            if (actionStates[trigger.data.personEmail] < 6) {
+                actionStates[trigger.data.personEmail] += 1
+                
+                var title = ""
+                
+                switch(actionStates[trigger.data.personEmail]) {
+                    case 2:
+                        reports[trigger.data.personEmail].accomplishedTask = message.text
+                        title = "**Did you meet yesterday's goals?**"
+                        break;
+                    case 3:
+                        reports[trigger.data.personEmail].isAchievedGoal = message.text == 'yes' || message.text == 'yep'
+                        title = "**What are your goals for today?**"
+                        break;
+                    case 4:
+                        reports[trigger.data.personEmail].goal = message.text
+                        title = "**Any obstacles impeding your progress?**"
+                        break;
+                    case 5:
+                        if (message.text == 'no' || message.text == 'nope' || message.text == 'not') {
+                            reports[trigger.data.personEmail].obstacle = ""
+                        } else {
+                            reports[trigger.data.personEmail].obstacle = message.text
+                        }
+                        
+                        let currentDate = new Date()
+                        title = `Thanks, <@personEmail: ${message.personEmail}>! You're checked in for ${currentDate.toString('MMM dd')}\n\nAccomplish: ${reports[trigger.data.personEmail].accomplishedTask}\n\nMet goal: ${reports[trigger.data.personEmail].isAchievedGoal}\n\nGoals: ${reports[trigger.data.personEmail].goal}\n\nObstacles: ${reports[trigger.data.personEmail].obstacle}\n`
+                        
+                        axios.post('http://afc62129.ngrok.io/api/reports/create', reports[trigger.data.personEmail])
+                            .catch((err) => {
+                                console.log(err)
+                            })
+                }
+    
+                spark.createMessage(message.roomId, title, { "markdown":true }, (err, message) => {})
+            }
         }
-    });
+    }
 });
 
-//
-// Welcome message 
-// sent as the bot is added to a Room
-//
 bot.onEvent("memberships", "created", function (trigger) {
-    var newMembership = trigger.data; // see specs here: https://developer.ciscospark.com/endpoint-memberships-get.html
+    var newMembership = trigger.data;
     if (newMembership.personId != bot.interpreter.person.id) {
-        // ignoring
         console.log("new membership fired, but it is not us being added to a room. Ignoring...");
         return;
     }
@@ -51,8 +102,8 @@ bot.onEvent("memberships", "created", function (trigger) {
                     console.log("WARNING: could not post Mention message to room: " + trigger.data.roomId);
                     return;
                 }
-            });
+            })
         }      
-    }); 
-});
+    }) 
+})
 
